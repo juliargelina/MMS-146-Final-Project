b3a6e75a4cadd64da813b4bd5b20273b349a6d49
YANA <kpaguinaldo@up.edu.ph>
2025-08-25 21:32:01 +0800

    Renamed file extensions with ".py"

--- Diary System v2.py
+++ Diary System v2.py
@@ -1,4 +1,4 @@
-from DiaryEntry import  DiaryEntry
+from DiaryEntry import DiaryEntry
 
 class DiarySystem:
     '''Represents the system of the diary, includes entries and encryption key'''
@@ -13,20 +13,21 @@ class DiarySystem:
         self.entries = entries
         self.encryption_key = encryption_key
 
-    def load_entry(self, entry_id):
+    def load_entry(self):
         ''' Displays a diary entry '''
-        entries = {}
-        entry_id = entries.items()
-
-        # for key, value in entries.items():
-        #     print (f"{key}: {value}") SHOULD GET SPECIFIC ENTRY
-    
+        return self.entries
+        # entries = {}
+        # entry_id = entries.keys()
+        # entries.get({entries.values()})
+        #need to get content for each entry
+        
     def save_entry (self):
         ''' Save a diary entry'''
         
 
-    def update_entry (self):
+    def update_entry (self, updated_entry):
         ''' Update a diary entry'''
+        self.entries = updated_entry
     
     def add_entry (self):
         ''' Add a diary entry'''
@@ -36,3 +37,15 @@ class DiarySystem:
 
     def change_encryption_key (self):
         ''' Change the encryption key'''
+
+
+
+#testing eme
+
+ds1 = DiarySystem(entries= "Slay", encryption_key="fesdmklvdsjlg")
+
+print("My entry: ", ds1.load_entry())
+
+ds1.update_entry("Not groovy")
+
+print("My entry: ", ds1.load_entry())
